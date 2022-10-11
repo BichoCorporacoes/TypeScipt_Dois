9b37404ca16ee5fd1e6a894b265f5e57a5e349ee
ZeroPirata <gabriel0gsbn@gmail.com>
2022-10-10 23:49:13 -0300

    Refactor - Escolha entre Dependente e Titular

--- atvii-atlantis/src/ts/processos/tipoEditarCliente.ts
+++ atvii-atlantis/src/ts/processos/tipoEditarCliente.ts
@@ -1,53 +1,28 @@
 import Processo from "../abstracoes/processo";
-import Armazem from "../dominio/armazem";
 import MenuTipoEditarClientes from "../menus/menuTipoEditarCliente";
-import Cliente from "../modelos/cliente";
 import EditarCliente from "./editarCliente";
+import EditarDependente from "./editarDependente";
 
-export default class TipoEditarCliente extends Processo {
-    private clientes: Cliente[]
-    constructor() {
-        super()
-        this.menu = new MenuTipoEditarClientes()
-        this.clientes = Armazem.InstanciaUnica.Clientes
-        this.execucao = true
+export default class TipoCadastroCliente extends Processo {
+  constructor() {
+    super();
+    this.menu = new MenuTipoEditarClientes();
+  }
+  processar(): void {
+    this.menu.mostrar();
+    this.opcao = this.entrada.receberNumero("Qual opção desejada?");
+
+    switch (this.opcao) {
+      case 1:
+        this.processo = new EditarCliente();
+        this.processo.processar();
+        break;
+      case 2:
+        this.processo = new EditarDependente();
+        this.processo.processar();
+        break;
+      default:
+        console.log("Opção não entendida :(");
     }
-    processar(): void {
-        console.clear()
-        let nome = this.entrada.receberTexto("Para começar a editar o cliente, forneça o nome: ")
-        this.clientes.filter(alvo => {
-            if (alvo.Nome == nome) {
-                while (this.execucao) {
-                    this.menu.mostrar()
-                    this.opcao = this.entrada.receberNumero('Qual opção desejada?')
-                    switch (this.opcao) {
-                        case 1:
-                            this.processo = new EditarCliente(alvo, 'nomes')
-                            this.processo.processar()
-                            this.execucao = false
-                            break;
-                        case 2:
-                            this.processo = new EditarCliente(alvo, 'datas')
-                            this.processo.processar()
-                            break;
-                        case 3:
-                            this.processo = new EditarCliente(alvo, 'endereço')
-                            this.processo.processar()
-                            break;
-                        case 4:
-                            this.processo = new EditarCliente(alvo, 'documentos')
-                            this.processo.processar()
-                            break;
-                        case 0:
-                            this.execucao = false
-                            break
-                        default:
-                            console.log('Opção não entendida :(')
-                    }
-                }
-            } else {
-                console.log(`Cliente não encontrado, tente novamente`);
-            }
-        })
-    }
+  }
 }
