57257b79c2e8f41dc50f97884eac4df69ea78d55
ZeroPirata <gabriel0gsbn@gmail.com>
2022-10-10 23:44:05 -0300

    Style - Identação, Nomes das variaveis ajustados

--- atvii-atlantis/src/ts/processos/listagemDepedente.ts
+++ atvii-atlantis/src/ts/processos/listagemDepedente.ts
@@ -5,24 +5,27 @@ import Impressor from "../interfaces/impressor";
 import Cliente from "../modelos/cliente";
 
 export default class ListagemDepedente extends Processo {
-    private clientes: Cliente[]
-    private impressor!: Impressor
-    constructor() {
-        super()
-        this.clientes = Armazem.InstanciaUnica.Clientes
-    }
-    processar(): void {
-        console.clear()
-        let titular = this.entrada.receberTexto(`Digite o nome do titular completo...`)
-        console.log('Iniciando a listagem dos clientes dependentes...')
-        this.clientes.filter(alvo => {
-            if (alvo.Nome == titular) {
-                alvo.Dependentes.forEach(dependentes => {
-                    this.impressor = new ImpressaorDepedente(dependentes)
-                    console.log(this.impressor.imprimir())
-                })
-            }
-        })
-
-    }
+  private clientes: Cliente[];
+  private impressor!: Impressor;
+  constructor() {
+    super();
+    this.clientes = Armazem.InstanciaUnica.Clientes;
+  }
+  processar(): void {
+    console.clear();
+    let titular = this.entrada.receberTexto(
+      `Digite o CPF do titular completo...`
+    );
+    console.log("Iniciando a listagem dos clientes dependentes...");
+    this.clientes.map((clienteMap) => {
+      clienteMap.Documentos.filter((docFilter) => {
+        if (docFilter.Numero === titular) {
+          clienteMap.Dependentes.forEach((dependentes) => {
+            this.impressor = new ImpressaorDepedente(dependentes);
+            console.log(this.impressor.imprimir());
+          });
+        }
+      });
+    });
+  }
 }
